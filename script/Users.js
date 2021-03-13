b780dce876dd636aa189001cd6cf1b3d13f22103
PodanevDmitriy <podaneovd@mail.ru>
2021-03-13 13:50:46 +0300

    add connect-btn

--- script/Users.js
+++ script/Users.js
@@ -1,21 +1,32 @@
 const URLrequest = "https://jsonplaceholder.typicode.com/users";
+const tbl = document.getElementById("users");
+let btn_connect = document.getElementById("connect");
 
+btn_connect.onclick = () => {
 
-fetch(URLrequest, {method: "GET"})
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        console.log(data);
-        data.map(item => {
-            console.log(item);
-            let listItem = document.createElement("li");
-            listItem.className ="userCheck";
-            listItem.innerHTML ='<div style="width: 15px; height: 15px;background: green;border-radius: 10em; margin-right: 30px;"></div>' + item.name; //todo проверять на active, чтобы вызывать цвет в html
-            document.getElementById("users").appendChild(listItem);
+    while (tbl.firstChild) {
+        tbl.removeChild(tbl.firstChild);
+    }
 
+    fetch(URLrequest, {method: "GET"})
+        .then(response => {
+            return response.json()
         })
-    });
+        .then(data => {
+
+            data.map(item => {
+
+                let listItem = document.createElement("li");
+                listItem.className = "userCheck";
+                listItem.innerHTML = '<div style="width: 15px; height: 15px;background: green;border-radius: 10em; margin-right: 30px;"></div>' + item.name; //todo проверять на active, чтобы вызывать цвет в html
+                document.getElementById("users").appendChild(listItem);
+
+            })
+        });
+};
+
+
+
 
 
 
